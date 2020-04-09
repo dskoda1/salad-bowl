d840cd6ee484e889e731c391d65bf387d3c35621
David Skoda <dskoda1@binghamton.edu>
2020-04-09 07:23:52 -0400

    join room functionality. create redirect is broken

--- app/containers/SaladBowl/index.js
+++ app/containers/SaladBowl/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import { Container, Row } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import NounForm from './NounForm';
+import { useSelector } from 'react-redux';
+import GameManager from './GameManager';
+import GamePlayer from './GamePlayer';
+import { Row } from 'reactstrap';
 
 const SaladBowl = () => {
   const { room } = useParams();
+  const roomData = useSelector((state) => state.room);
 
+  let gameAwareComponent = <GamePlayer />;
+  if (room === roomData.id) {
+    gameAwareComponent = <GameManager />;
+    // Owner of the room
+  }
   return (
     <Container>
       <Row>Room: {room}</Row>
+      <Row>{gameAwareComponent}</Row>
       <Row>
         <NounForm />
       </Row>
--- app/redux/actions/room.js
+++ app/redux/actions/room.js
@@ -3,6 +3,10 @@ export const CREATE_ROOM = 'ROOM_CREATE_ROOM';
 export const CREATE_ROOM_ERROR = 'ROOM_CREATE_ROOM_ERROR';
 export const CREATE_ROOM_SUCCESS = 'ROOM_CREATE_ROOM_SUCCESS';
 
+export const JOIN_ROOM = 'ROOM_JOIN_ROOM';
+export const JOIN_ROOM_ERROR = 'ROOM_JOIN_ROOM_ERROR';
+export const JOIN_ROOM_SUCCESS = 'ROOM_JOIN_ROOM_SUCCESS';
+
 export const refreshRoomId = () => ({
   type: REFRESH_ROOM_ID,
 });
@@ -22,4 +26,21 @@ export const createRoomErrorAction = (error) => ({
 
 export const createRoomSuccessAction = () => ({
   type: CREATE_ROOM_SUCCESS,
+});
+
+export const joinRoomAction = (roomId, userName) => ({
+  type: JOIN_ROOM,
+  payload: {
+    roomId,
+    userName,
+  },
+});
+
+export const joinRoomErrorAction = (error) => ({
+  type: JOIN_ROOM_ERROR,
+  payload: error,
+});
+
+export const joinRoomSuccessAction = () => ({
+  type: JOIN_ROOM_SUCCESS,
 });
